a70ecff8775f824b2823aff1572dcd4b4f9ae62d
gabmellofull <gmellocorp@gmail.com>
2021-10-27 19:54:43 -0300

    add codigo do projeto

---
+++ src/app/shared/list-titles/list-titles.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-titles',
+  templateUrl: './list-titles.component.html',
+  styleUrls: ['./list-titles.component.css']
+})
+export class ListTitlesComponent implements OnInit {
+
+  @Input() title: string | undefined;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -0,0 +1,17 @@
+import { style } from "@angular/animations";
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'app-nav-bar',
+    templateUrl: './nav-bar.component.html',
+    styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent implements OnInit{
+    
+    constructor(){
+    }
+
+    ngOnInit(): void{
+
+    }
+}
